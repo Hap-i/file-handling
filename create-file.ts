5755a63ab264045af76278e92616fd6be772ca8a
suvendu sekhar sahoo <suvendusekharsahoo5@gmail.com>
2022-08-13 12:58:44 +0530

    copy dir code

--- create-file.ts
+++ create-file.ts
@@ -1,3 +1,18 @@
-var fs = require("fs");
+import * as fse from "fs-extra";
 
-fs.writeFile();
+console.log(__dirname);
+
+const srcDir = "./static";
+const destDir = "./generated-code";
+
+// copy dir
+async function copyFiles() {
+  try {
+    await fse.copy(srcDir, destDir);
+    console.log("success!");
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+copyFiles();
